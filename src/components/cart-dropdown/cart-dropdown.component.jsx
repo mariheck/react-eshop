1764278653c5212aa779dc7c259beafe48103621
Marine Heckler <marine.heckler@gmail.com>
2020-06-16 21:06:29 +0200

    Add checkout page and checkout item component.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className="cart-dropdown">
         <div className="cart-items">
             {cartItems.length ? (
@@ -20,7 +22,14 @@ const CartDropdown = ({ cartItems }) => (
                 <span className="empty-message">Your cart is empty</span>
             )}
         </div>
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+        <CustomButton
+            onClick={() => {
+                history.push('/checkout');
+                dispatch(toggleCartHidden());
+            }}
+        >
+            GO TO CHECKOUT
+        </CustomButton>
     </div>
 );
 
@@ -28,4 +37,4 @@ const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));

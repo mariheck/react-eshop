95e509321221a049b1a7e2237e51dd39425a04b3
Marine Heckler <marine.heckler@gmail.com>
2020-06-16 19:28:04 +0200

    Add dropdown cart celement.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,14 +6,16 @@ import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link className="logo-container" to="/">
             <Logo className="logo" />
         </Link>
-
         <div className="options">
             <Link className="option" to="/shop">
                 SHOP
@@ -30,12 +32,15 @@ const Header = ({ currentUser }) => (
                     SIGN IN
                 </Link>
             )}
+            <CartIcon />
         </div>
+        {hidden ? null : <CartDropdown />}
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);

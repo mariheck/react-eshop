87b329c66ae0a45f87384d331fc8d7529726784a
Marine Heckler <marine.heckler@gmail.com>
2020-07-03 18:10:16 +0200

    Update stylings for responsive behavior - pt1.

--- client/src/components/custom-button/custom-button.styles.jsx
+++ client/src/components/custom-button/custom-button.styles.jsx
@@ -3,12 +3,11 @@ import styled, { css } from 'styled-components';
 const buttonStyles = css`
     background-color: black;
     color: white;
-    border: none;
+    border: 1px solid black;
 
     &:hover {
         background-color: white;
         color: black;
-        border: 1px solid black;
     }
 `;
 
@@ -20,17 +19,16 @@ const invertedButtonStyles = css`
     &:hover {
         background-color: black;
         color: white;
-        border: none;
     }
 `;
 
 const googleSignInStyles = css`
     background-color: #4285f4;
     color: white;
+    border: none;
 
     &:hover {
         background-color: #357ae8;
-        border: none;
     }
 `;
 
